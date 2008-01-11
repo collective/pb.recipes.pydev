a4839fcc42d7bb7e71b357d12108db113a77cce8
Tiberiu Ichim <tibi@pixelblaster.ro>
2008-01-11 16:22:12 +0000

    restructure to use minidom

--- src/pb/recipes/pydev/README.txt
+++ src/pb/recipes/pydev/README.txt
@@ -7,6 +7,8 @@ WARNING: this recipe overwrites the .pydevproject file with a simplistic version
 make a backup or fix/extend the recipe to properly handle the file. For the
 moment, it does everything I need.
 
+    >>> import os
+
 This recipe is about generating a `.pydevproject` file, which is used by Eclipse
 and PyDev to hold a list of folders which hold Python packages (for code
 completition, auto-import and so on). The idea is to fill this file with paths
@@ -22,18 +24,41 @@ A full recipe would include the following options:
 	...
 	... [pydev]
 	... recipe = pb.recipes.pydev
-	... pydevproject_path = ${buildout:directory}/.pydevproject_new
+	... pydevproject_path = ${buildout:directory}/.pydevproject_test
 	... extra_paths = /something/else
 	... target_python = python2.4
 	... eggs = pb.recipes.pydev
 	... """)
+	
+We need a working .pydevproject file. This recipe won't generate a new one. 
+I'll use this project's project file for testing.
+
+    >>> fc = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    ... <?eclipse-pydev version="1.0"?>
+    ... <pydev_project>
+    ... <pydev_pathproperty name="org.python.pydev.PROJECT_SOURCE_PATH">
+    ... <path>/pb.recipes.pydev/src</path>
+    ... </pydev_pathproperty>
+    ... <pydev_property name="org.python.pydev.PYTHON_PROJECT_VERSION">python 2.4</pydev_property>
+    ... <pydev_pathproperty name="org.python.pydev.PROJECT_EXTERNAL_SOURCE_PATH">
+    ... <path>/home/tibi/workspace3.3/pb.recipes.pydev/eggs/zc.buildout-1.0.0b31-py2.4.egg</path>
+    ... <path>/home/tibi/workspace3.3/pb.recipes.pydev/eggs/zc.recipe.egg-1.0.0-py2.4.egg</path>
+    ... </pydev_pathproperty>
+    ... </pydev_project>"""
+    >>> pf_path = os.path.join(sample_buildout, '.pydevproject_test')
+    >>> f = open(pf_path, 'w')
+    >>> f.write(fc)
+    >>> f.close()
+
+Now we can run the buildout
+	
 	>>> print system(buildout) #doctest: +NORMALIZE_WHITESPACE
 	Installing pydev.
 
-Let's verify the written `.pydevproject_new` file, the `extra_paths` option
+Let's verify the written `.pydevproject_test` file, the `extra_paths` option
 
 	>>> import os
-	>>> data = open(os.path.join(sample_buildout, '.pydevproject_new')).read()
+	>>> data = open(os.path.join(sample_buildout, '.pydevproject_test')).read()
 	>>> '<path>/something/else</path>' in data
 	True
 
--- src/pb/recipes/pydev/__init__.py
+++ src/pb/recipes/pydev/__init__.py
@@ -1,12 +1,13 @@
+from xml.dom.minidom import parse, parseString
 import os
-import zc.recipe.egg
+import zc.recipe.egg 
 
 class PyDev(object):
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
         wd = self.buildout['buildout']['directory']
 
-        self._fpath = self.options.get('pydevproject_path',
+        self._fpath = self.options.get('pydevproject_path', 
                                        os.path.join(wd, '.pydevproject'))
         self._python = options.get('target_python', 'python2.4')
         self._extra_paths = options.get('extra_paths', '').split('\n')
@@ -17,56 +18,21 @@ class PyDev(object):
         egg_names, ws = egg.working_set(self._app_eggs)
         egg_paths = ws.entries + self._extra_paths
         egg_paths = [p for p in egg_paths if p.strip() != '']   #strip empty paths
+        
+        pydev_dom = parse(self._fpath)
+        nodes = pydev_dom.getElementsByTagName('pydev_pathproperty')
+        prop = filter( lambda node: (node.getAttribute('name') == 
+                            'org.python.pydev.PROJECT_EXTERNAL_SOURCE_PATH'),
+                       nodes
+                   )[0]
+        clone = prop.cloneNode(False)
 
-        _dev = self.buildout['buildout'].get('develop', './')
-        _dir = self.buildout['buildout']['directory']
-
-        src_paths = []
-        #XXX: stupid way of determining src folders, should replace
-        #source folders should be relative to workspace
-        #we assume the buildout is run directly in the project, in the workspace
-        #example path:
-        #/home/me/workspace/MyProject/src
-        for p in _dev.splitlines():
-            #XXX: probably it won't work with multiple dev paths, especially
-            #if they're placed outside the current project. Solutions??
-            if not p.endswith('src'):
-                p = os.path.join(_dir, p, './src')
-                p = os.path.normpath(p)
-            if p in egg_paths:
-                #take out this project's src folder from the egg paths
-                egg_paths.pop(egg_paths.index(p))
-            src_paths.append(p)
-
-        #we need a path that is "relative" to the workspace location
-        #in Eclipse it will look like:
-        #/MyProject/src
-        #we assume these are the last two elements in the path
-        _res = []
-        for p in src_paths:
-            buf = []
-            while True:
-                frags = os.path.split(p)
-                buf.append(frags[1])
-                p = frags[0]
-                if (len(p) == 0) or (p == os.path.sep):
-                    break
-            buf.reverse()
-            _res.append('/%s' % '/'.join(buf[-2:]))  #XXX: is / ok as path separator for Eclipse??
-
-        src_paths = _res
-
-        pydev_paths = '\n'.join(['<path>%s</path>' % p for p in egg_paths])
-        src_paths = '\n'.join(['<path>%s</path>' % p for p in src_paths])
-
-        content = template % {'paths':pydev_paths,
-                              'target_python':self._python,
-                              'src_path':src_paths
-                             }
-
-        f = open(self._fpath, 'w')
-        f.write(content)
-        f.close()
+        for p in egg_paths:
+            node = pydev_dom.createElement('path')
+            node.appendChild(pydev_dom.createTextNode(p))
+            clone.appendChild(node)
+            
+        open(self._fpath, 'w').write(pydev_dom.toxml())
         return self._fpath
 
     update = install
