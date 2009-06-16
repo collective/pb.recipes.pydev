2a05b1ab6898c70d805665c9bbd24abd7cf28339
Jonathan Riboux <jonathan.riboux@quadra-informatique.fr>
2009-06-16 14:30:31 +0000

    Incremented version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup (
                         + "\n\n" +
                         read('CHANGES.txt')
     ), 
-    version='0.4.3', 
+    version='0.4.4', 
     author = "Tiberiu Ichim - Pixelblaster SRL", 
     author_email = "tibi@pixelblaster.ro", 
     license = "BSD", 
